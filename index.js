96ea5509cae57cf5ad8038121a2ec37344b0b231
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-10-17 11:34:37 +0600

    crud oparation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
@@ -21,15 +21,84 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
     await client.connect();
+
+    // jobs collections
+    const jobCollection = client.db("career_code").collection("jobs");
+    const applicaitonsCollection = client
+      .db("career_code")
+      .collection("applicaitons");
+
+    // jobs get methords
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    // applicaitons api
+
+    app.get("/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        applicat: email,
+      };
+      const result = await applicaitonsCollection.find(query).toArray();
+
+      // bad way
+
+      for (const application of result) {
+        const jobId = application.jobId;
+        const jobQuery = { _id: new ObjectId(jobId) };
+        const job = await jobCollection.findOne(jobQuery);
+        application.company = job.company;
+        application.title = job.title;
+        application.company_logo = job.company_logo;
+        application.jobType = job.jobType;
+      }
+
+      res.send(result);
+    });
+
+    app.post("/applications", async (req, res) => {
+      const applicaiton = req.body;
+      console.log(applicaiton);
+      const result = await applicaitonsCollection.insertOne(applicaiton);
+      res.send(result);
+    });
+
+    app.delete("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+
+      try {
+        const query = { _id: new ObjectId(id) };
+        const result = await applicaitonsCollection.deleteOne(query); 
+
+        if (result.deletedCount > 0) {
+          res.send({ success: true, message: "Application deleted" });
+        } else {
+          res
+            .status(404)
+            .send({ success: false, message: "Application not found" });
+        }
+      } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
     // await client.close();
   }
 }
