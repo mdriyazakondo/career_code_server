7460125c6aa72367cc37981c081aff0f07968994
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-10-18 19:43:25 +0600

    jwt auth

--- index.js
+++ index.js
@@ -1,12 +1,73 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+// firebase admin token
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase-admin-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const logger = (req, res, next) => {
+  console.log("inside the logger middleware");
+  next();
+};
+
+//verifyToken
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies.token;
+  console.log("cooke in the middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  // verify token
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+//verifyToken firebase token
+const verifyFirebaseToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const userInfo = await admin.auth().verifyIdToken(token);
+
+    req.tokenEmail = userInfo.email;
+
+    next();
+  } catch (error) {
+    console.error("Firebase token verification failed:", error);
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 //conact databse
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rxqtlel.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,6 +90,22 @@ async function run() {
       .db("career_code")
       .collection("applicaitons");
 
+    // jwt web tooken
+
+    app.post("/jwt", async (req, res) => {
+      const userData = req.body;
+
+      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "1d",
+      });
+      // console.log(token);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+      res.send({ success: "true" });
+    });
+
     // jobs get methords
 
     app.get("/jobs", async (req, res) => {
@@ -41,7 +118,7 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    
+
     app.get("/jobs/applicaitons", async (req, res) => {
       const email = req.query.email;
       const query = { hr_email: email };
@@ -66,34 +143,50 @@ async function run() {
 
     app.post("/jobs", async (req, res) => {
       const newJob = req.body;
-      console.log(newJob);
+
       const result = await jobCollection.insertOne(newJob);
       res.send(result);
     });
 
     // applicaitons api
 
-    app.get("/applications", async (req, res) => {
-      const email = req.query.email;
-      const query = {
-        applicat: email,
-      };
-      const result = await applicaitonsCollection.find(query).toArray();
+    app.get(
+      "/applications",
+      logger,
+      verifyToken,
+      verifyFirebaseToken,
+      async (req, res) => {
+        const email = req.query.email;
+        // console.log("inside the cookies", req.cookies);
 
-      // bad way
+        if (req.tokenEmail != email) {
+          return res.status(404).send({ message: "forbidden access" });
+        }
 
-      for (const application of result) {
-        const jobId = application.jobId;
-        const jobQuery = { _id: new ObjectId(jobId) };
-        const job = await jobCollection.findOne(jobQuery);
-        application.company = job.company;
-        application.title = job.title;
-        application.company_logo = job.company_logo;
-        application.jobType = job.jobType;
+        if (email !== req.decoded.email) {
+          return res.status(404).send({ message: "forbidden access" });
+        }
+
+        const query = {
+          applicat: email,
+        };
+        const result = await applicaitonsCollection.find(query).toArray();
+
+        // bad way
+
+        for (const application of result) {
+          const jobId = application.jobId;
+          const jobQuery = { _id: new ObjectId(jobId) };
+          const job = await jobCollection.findOne(jobQuery);
+          application.company = job.company;
+          application.title = job.title;
+          application.company_logo = job.company_logo;
+          application.jobType = job.jobType;
+        }
+
+        res.send(result);
       }
-
-      res.send(result);
-    });
+    );
 
     app.get("/applicaitons/job/:job_id", async (req, res) => {
       const job_id = req.params.job_id;
@@ -104,7 +197,7 @@ async function run() {
 
     app.post("/applications", async (req, res) => {
       const applicaiton = req.body;
-      console.log(applicaiton);
+
       const result = await applicaitonsCollection.insertOne(applicaiton);
       res.send(result);
     });
@@ -158,6 +251,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`serve is running port : http://localhost:${port}/`);
 });
-//DOAVVDemAg6lGiDR
-
-//career_code_project
